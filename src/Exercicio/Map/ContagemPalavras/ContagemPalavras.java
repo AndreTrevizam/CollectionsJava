254ad39482a77790f36d296d531fc2057eb130df
AndreTrevizam <andretrevizam264@gmail.com>
2024-09-27 15:37:43 -0300

    pesquisa com maps

--- src/Exercicio/Map/ContagemPalavras/ContagemPalavras.java
+++ src/Exercicio/Map/ContagemPalavras/ContagemPalavras.java
@@ -14,7 +14,11 @@ public class ContagemPalavras {
 
     //metodos
     public void adicionarPalavra(String palavra, Integer contagem) {
-        contarPalavrasMap.put(palavra, contagem);
+        if (contarPalavrasMap.containsKey(palavra)) {
+            contarPalavrasMap.put(palavra, contarPalavrasMap.get(palavra) + contagem);
+        } else {
+            contarPalavrasMap.put(palavra, contagem);
+        }
     }
 
     public void removerPalavra(String palavra) {
@@ -23,5 +27,31 @@ public class ContagemPalavras {
         } else {
             System.out.println("Essa palavra não esta presente no Map");
         }
+    }
+
+    public void exibirContagemPalavras() {
+        System.out.println(contarPalavrasMap);
+    }
+
+    public String encontrarPalavraMaisFrequente() {
+        String palavraMaisFrequente = null;
+        int maiorContagem = 0;
+
+        for (Map.Entry<String, Integer> entrada : contarPalavrasMap.entrySet()) {
+            if (entrada.getValue() > maiorContagem) {
+                palavraMaisFrequente = entrada.getKey();
+                maiorContagem = entrada.getValue();
+            }
+        }
+        return palavraMaisFrequente;
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras contagemPalavras = new ContagemPalavras();
+        contagemPalavras.adicionarPalavra("Olá", 1);
+        contagemPalavras.adicionarPalavra("Mundo", 1);
+        contagemPalavras.adicionarPalavra("Olá", 1);
+        System.out.println(contagemPalavras.encontrarPalavraMaisFrequente());
+        contagemPalavras.exibirContagemPalavras();
     }
 }
